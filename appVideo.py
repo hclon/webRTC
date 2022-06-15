18827f91b4a1652075469a31089e6eb1da950797
hclon <hmedna@gmail.com>
2022-06-15 17:48:03 +0100

    st

--- appVideo.py
+++ appVideo.py
@@ -1,12 +1,20 @@
 import streamlit as st
-import pandas as pd
-import qrcode
-import cv2
-import numpy as np
 from streamlit_webrtc import webrtc_streamer
+import av
+import cv2
 
-st.set_page_config(page_title="Dashboard", layout ="wide")
+st.title("Hello PyConJP 2021!🤗")
 
-st.write("webRTC")  
+webrtc_streamer(key="sample")
 
-webrtc_streamer(key="qr")
+
+class VideoProcessor:
+     def recv(self, frame):
+         img = frame.to_ndarray(format="bgr24")
+
+         img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+         return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+webrtc_streamer(key="edge", video_processor_factory=VideoProcessor)
