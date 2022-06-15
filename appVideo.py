18e5fb0ca8743cb8f3daa5c9310e5e82067efa08
hclon <hmedna@gmail.com>
2022-06-15 11:17:21 +0100

    $2

--- appVideo.py
+++ appVideo.py
@@ -3,10 +3,6 @@ import pandas as pd
 import qrcode
 import cv2
 import numpy as np
-import pyzbar.pyzbar as pyzbar
-from PIL import Image
-import datetime
-import time
 from streamlit_webrtc import webrtc_streamer
 
 st.set_page_config(page_title="Dashboard", layout ="wide")
