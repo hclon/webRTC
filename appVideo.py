02b5929a6d8ace3108513fd556d2bda0a5742a59
hclon <hmedna@gmail.com>
2022-06-15 12:02:33 +0100

    $1

--- appVideo.py
+++ appVideo.py
@@ -4,19 +4,9 @@ import qrcode
 import cv2
 import numpy as np
 from streamlit_webrtc import webrtc_streamer
-import av
 
 st.set_page_config(page_title="Dashboard", layout ="wide")
 
 st.write("webRTC")  
 
-
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-
-    flipped = img[::-1,:,:]
-
-    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
-
-
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+webrtc_streamer(key="example")
